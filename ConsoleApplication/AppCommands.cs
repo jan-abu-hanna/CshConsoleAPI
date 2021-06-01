d03d65d68b7e0487ab3fc6be6ce5277fdd6ef79f
farouk shtewee <farok95@hotmail.com>
2021-06-01 16:18:09 +0300

    First Push

--- ConsoleApplication/AppCommands.cs
+++ ConsoleApplication/AppCommands.cs
@@ -15,10 +15,10 @@ namespace CshConsoleAPI
 	{
 		public const string CMD_PROMPED = "\n>";
 		public const string CMD_ECHO = "echo";
-
-
-
-
+		public const string CMD_ADD = "add";
+		public const string CMD_SUB = "sub";
+		public const string CMD_MULT = "mult";
+		public const string CMD_DIV = "div";
 
 		/***
 		* Echo demonstrates a command execution function.
@@ -31,5 +31,88 @@ namespace CshConsoleAPI
 			}
 			return (true);
 		}
+
+
+		public static bool CommandADD(string[] parameters)
+		{
+			int sum = 0;
+			foreach (string param in parameters)
+			{
+				sum += int.Parse(param);
+			}
+			Console.WriteLine(sum);
+			return (true);
+		}
+
+
+		public static bool CommandSUB(string[] parameters)
+		{
+			int sum = 0;
+			bool first = true;
+
+
+			foreach (string param in parameters)
+			{
+				if(first)
+				{
+					first = false;
+					sum -= int.Parse(param);
+					sum *= -1;
+
+				}
+				else
+				sum -= int.Parse(param);
+			}
+			Console.WriteLine(sum);
+			return (true);
+		}
+
+
+		public static bool CommandMULT(string[] parameters)
+		{
+			int mult = 1;
+			bool first = true;
+
+
+			foreach (string param in parameters)
+			{
+				if (first)
+				{
+					first = false;
+					mult *= int.Parse(param);
+					
+
+				}
+				else
+					mult *= int.Parse(param);
+			}
+			Console.WriteLine(mult);
+			return (true);
+		}
+
+		
+		public static bool CommandDIV(string[] parameters)
+		{
+			double div = 0;
+			bool first = true;
+
+
+			foreach (string param in parameters)
+			{
+				if (first)
+				{
+					first = false;
+					div = double.Parse(param);
+
+
+				}
+				else
+					div /= double.Parse(param);
+			}
+			Console.WriteLine(div);
+			return (true);
+		}
+
+
 	}
 }
